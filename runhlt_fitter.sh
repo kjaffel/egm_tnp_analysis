8316fd0da2225a18c2b21fc06bd936b56cc3e866
kjaffel <khawla.jaffel@cern.ch>
2021-07-23 16:10:46 +0200

    update bash script for  running TnP cmd

--- runhlt_fitter.sh
+++ runhlt_fitter.sh
@@ -1,10 +1,36 @@
-#python tnpEGM_fitter.py etc/config/settings_et_HLT.py --flag passTrackIsoLeg1 --doCutCount --onlyDoPlot --plotX et
+#!/bin/bash
+variables=(
+'et'
+'eta'
+'nVtx')
 
-variables=('eta' 'nvtx' 'et')
-procedure=('--checkBins --onlyDoPlot --plotX' '--createBins --onlyDoPlot --plotX' '--doCutCount --onlyDoPlot --plotX')
+flags=(
+'pass_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVLLeg2' 
+'pass_HLT_Ele27_WPTight_Gsf'
+'pass_HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVLLeg1L1match'
+'pass_HLT_DoubleEle33_CaloIdL_MWSeedLegL1match'
+'pass_HLT_DoubleEle33_CaloIdL_MWUnsLeg')
 
 for var in ${variables[*]}; do
-    for step in ${procedure[*]}; do
-		python tnpEGM_fitter.py etc/config/settings_${var}_HLT.py --flag passTrackIsoLeg1 $step $var
+    for flag in ${flags[*]}; do
+            echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+            echo " Tag-and-Probe working on : Efficency vs $var  for Flag = $flag"
+            echo " main-cmd :  python tnpEGM_fitter.py etc/config/HLTsettings/settings_${var}_HLT.py --flag ${flag} --doCutCount --onlyDoPlot --plotX ${var} --doHLT"
+            echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+
+            python tnpEGM_fitter.py etc/config/HLTsettings/settings_${var}_HLT.py --flag ${flag} --checkBins --doHLT 
+            python tnpEGM_fitter.py etc/config/HLTsettings/settings_${var}_HLT.py --flag ${flag} --createBins --doHLT
+            python tnpEGM_fitter.py etc/config/HLTsettings/settings_${var}_HLT.py --flag ${flag} --createHists --doHLT
+            python tnpEGM_fitter.py etc/config/HLTsettings/settings_${var}_HLT.py --flag ${flag} --doCutCount --onlyDoPlot --plotX ${var} --doHLT
+                # Nominal fit
+            #python tnpEGM_fitter.py etc/config/HLTsettings/settings_${var}_HLT.py --flag ${flag} --doFit
+                # MC fit to constrain alternate signal parameters [note this is the only MC fit that makes sense]
+            #python tnpEGM_fitter.py etc/config/HLTsettings/settings_${var}_HLT.py --flag ${flag} --doFit --mcSig --altSig
+                # Alternate signal fit (using constraints from previous fits)
+            #python tnpEGM_fitter.py etc/config/HLTsettings/settings_${var}_HLT.py --flag ${flag} --doFit --altSig
+                # Alternate background fit (using constraints from previous fits)
+            #python tnpEGM_fitter.py etc/config/HLTsettings/settings_${var}_HLT.py --flag ${flag} --doFit --altBkg
+                # egm txt ouput file. Once all fits are fine, put everything in the egm format txt file
+            #python tnpEGM_fitter.py etc/config/HLTsettings/settings_${var}_HLT.py --flag ${flag} --sumUp
     done
 done
